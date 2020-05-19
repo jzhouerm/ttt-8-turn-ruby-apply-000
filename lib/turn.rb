95ecbdfe940892b0d92a7ab4a9a9380df64db8d7
JULIA ZHOU <jzhouerm@hotmail.com>
2020-05-19 19:52:59 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -23,7 +23,7 @@ end
 
 
 def valid_move?(board, index)
-  if index.between?(0,8) && position_taken?(board, index) == false
+  if index.between?(0,8) && !position_taken?(board, index)
     true
   else
     false
